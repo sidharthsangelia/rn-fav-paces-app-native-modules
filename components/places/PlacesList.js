c3813f2385d08af931eda8d6f9cd5239fa0152a0
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-08-22 22:34:37 +0530

    add: basic screen components

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import PlaceItem from "./PlaceItem";
 
-export default function PlacesList() {
+export default function PlacesList({ places }) {
+  if (!places || places.lenght === 0) {
+    return (
+      <View>
+        <Text>No places added yet. Please add places</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>PlacesList</Text>
+      <FlatList
+        data={places}
+        keyExtractor={(item) => {
+          item.id;
+        }}
+        renderItem={({ item }) => <PlaceItem place={item} />}
+      />
     </View>
-  )
+  );
 }
